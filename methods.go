8682ed19e96f3675aa7ad6bf610e437113277a7f
Afsal <thenameisafsalahamad@gmail.com>
2024-10-10 12:43:59 +0530

    pointer vs copy

--- methods.go
+++ methods.go
@@ -7,6 +7,11 @@ import "fmt"
 func main() {
 	user := User{"afsal", 20}
 	fmt.Println(user.getAge())
+	user.alterName("ahamad")
+	fmt.Println(user, "not changed")
+	var userptr *User = &user
+	userptr.rootAlterName("afsal ahamad") // altered because of the pointer
+	fmt.Println(user)                     // altered user
 }
 
 type User struct {
@@ -17,4 +22,13 @@ type User struct {
 // an object user of type User will have this method
 func (user User) getAge() int {
 	return user.age
+}
+
+func (user User) alterName(newName string) {
+	user.name = newName // didn't change my original age -> because only the copy of object is passed, hence we'll use pointer to sovle this problem
+	fmt.Println(user.name, "the new name")
+}
+
+func (user *User) rootAlterName(TheName string) {
+	user.name = TheName
 }
