230898dbf7f6c4baf3d37b1abc7d9ad5c506a473
Afsal <thenameisafsalahamad@gmail.com>
2024-10-10 17:28:18 +0530

    json using go

---
+++ createJsonData.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type Course struct {
+	Name     string
+	Price    int
+	password string
+	tags     []string
+}
+
+func main() {
+	encodeJson()
+}
+
+func encodeJson() {
+	// slice of structs
+	courses := []Course{
+		{"React", 999, "12345", []string{"Dev", "dev"}},
+		{"Flutter", 999, "12345", []string{"Dev", "dev"}},
+		{"Go", 999, "12345", []string{"Dev", "dev"}},
+		{"JS", 999, "12345", []string{"Dev", "dev"}},
+	}
+
+	// package the data as json data
+	// pass an interface here
+	finalJson, err := json.Marshal(courses)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("%s\n", finalJson)
+}
