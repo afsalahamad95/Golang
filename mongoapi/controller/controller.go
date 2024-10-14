4e001daab73e6b5b3dda0533e32fa7bf8755d161
Afsal <thenameisafsalahamad@gmail.com>
2024-10-14 10:20:47 +0530

    controllers over

--- mongoapi/controller/controller.go
+++ mongoapi/controller/controller.go
@@ -121,9 +121,24 @@ func CreateMovie(w http.ResponseWriter, r *http.Request) {
 
 func MarkAsWatched(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/x-www-form-urlencode")
-	w.Header().Set("Allow-control-Allow-Methods", "POST")
+	w.Header().Set("Allow-control-Allow-Methods", "PUT")
 
 	params := mux.Vars(r)
 	updateOneMovie(params["id"])
 	json.NewEncoder(w).Encode(params["id"])
+}
+
+func DeleteOneMovie(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/x-www-form-urlencode")
+	w.Header().Set("Allow-control-Allow-Methods", "DELETE")
+	params := mux.Vars(r)
+	deleteOneMovie(params["id"])
+	json.NewEncoder(w).Encode(params["id"])
+}
+
+func DeleteAllMovies(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/x-www-form-urlencode")
+	w.Header().Set("Allow-control-Allow-Methods", "DELETE")
+	deleteAllRecords()
+	json.NewEncoder(w).Encode("Deleted all movies")
 }
