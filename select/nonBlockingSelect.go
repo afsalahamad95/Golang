21aff7895c58e5f921f67d8686555d5593adb89b
Afsal <thenameisafsalahamad@gmail.com>
2024-10-16 14:27:12 +0530

    non blocking select

---
+++ select/nonBlockingSelect.go
@@ -0,0 +1,54 @@
+// Basic sends and receives on channels are blocking.
+// However, we can use `select` with a `default` clause to
+// implement _non-blocking_ sends, receives, and even
+// non-blocking multi-way `select`s.
+
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	messages := make(chan string)
+
+	// Here's a non-blocking receive. If a value is
+	// available on `messages` then `select` will take
+	// the `<-messages` `case` with that value. If not
+	// it will immediately take the `default` case.
+	select {
+	case msg := <-messages:
+		fmt.Println("received message", msg)
+	default:
+		fmt.Println("no message received")
+	}
+
+	// A non-blocking send works similarly. Here `msg`
+	// cannot be sent to the `messages` channel, because
+	// the channel has no buffer and there is no receiver.
+	// Therefore the `default` case is selected.
+	msg := "hi"
+	select {
+	case messages <- msg:
+		fmt.Println("sent message", msg)
+	default:
+		fmt.Println("no message sent")
+	}
+
+	// We can use multiple `case`s above the `default`
+	// clause to implement a multi-way non-blocking
+	// select. Here we attempt non-blocking receives
+	// on both `messages` and `signals`.
+	go func() {
+		time.Sleep(2 * time.Second)
+		messages <- "yowza"
+	}()
+	select {
+	case msg := <-messages:
+		fmt.Println("received message", msg)
+		// non blocking select will include default case
+		// default:
+		// fmt.Println("no activity")
+	}
+}
