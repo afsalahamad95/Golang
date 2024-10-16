c6a70fb173cacf04cd973e1b74ccffacf048991d
Afsal <thenameisafsalahamad@gmail.com>
2024-10-16 16:21:59 +0530

    timers

---
+++ timers/go.mod
@@ -0,0 +1,3 @@
+module timer.go
+
+go 1.23.1
---
+++ timers/timer.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	timer := time.NewTimer(1 * time.Second)
+	// afterfunc will execute the function after the timer expires
+	time.AfterFunc(1*time.Second, func() {
+		fmt.Println("Function executed after timeout")
+	})
+	fmt.Println("Timer fired")
+	<-timer.C // This blocks the execution of the program until the timer expires
+	fmt.Println("Timer expired")
+	time.Sleep(2 * time.Second)
+
+	// returns a channel that will send the current time after specified timeout without creating a timer
+	<-time.After(1 * time.Second)
+	fmt.Println("1 second passed")
+}
