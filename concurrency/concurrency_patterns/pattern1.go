55c3be02bc34ad0eb119a7f33bce862eae8eba0c
Afsal <thenameisafsalahamad@gmail.com>
2024-10-17 15:57:50 +0530

    for-select loops

---
+++ concurrency/concurrency_patterns/pattern1.go
@@ -0,0 +1,28 @@
+// for - select loop
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func doWork(done <-chan bool) {
+	// for-select loop
+	for {
+		select {
+		// the channel is used to terminate the loop
+		case <-done:
+			return
+		default:
+			fmt.Println("working")
+		}
+	}
+}
+func main() {
+	done := make(chan bool)
+	go doWork(done)
+	time.Sleep(3 * time.Second)
+	// stop after 3 seconds
+	close(done)
+
+}
--- go.mod
+++ go.mod
@@ -3,3 +3,10 @@ module apiModel.go
 go 1.23.1
 
 require github.com/gorilla/mux v1.8.1
+
+require (
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
+	golang.org/x/text v0.19.0 // indirect
+	gorm.io/gorm v1.25.12 // indirect
+)
